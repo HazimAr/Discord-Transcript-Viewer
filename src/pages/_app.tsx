7be305423f51cc155cd1ce99b6375a4a28fadd35
Hazim Arafa <hazimarafa80@gmail.com>
2022-01-25 22:46:40 -0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import theme from "@styles/theme";
 import { META } from "config";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { useRouter } from "next/router";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
