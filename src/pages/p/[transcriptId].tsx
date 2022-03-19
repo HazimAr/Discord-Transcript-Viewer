ff0c3e462aa0e7a02a4c5834cc830cd298d3f319
Hazim Arafa <hazimarafa80@gmail.com>
2022-03-19 14:12:16 -0700

    Create [transcriptId].tsx

---
+++ src/pages/p/[transcriptId].tsx
@@ -0,0 +1,38 @@
+import { Client } from "@notionhq/client";
+import { GetServerSidePropsContext } from "next";
+
+const notion = new Client({
+	auth: process.env.NOTION_TOKEN_OLD_PS,
+});
+
+export default function Home({ transcriptHtml }) {
+	return (
+		<div
+			id="transcript"
+			dangerouslySetInnerHTML={{ __html: transcriptHtml }}
+		></div>
+	);
+}
+
+export async function getServerSideProps({
+	params,
+}: GetServerSidePropsContext) {
+	const textArray = 
+	(
+		await notion.blocks.children.list({
+			block_id: params.transcriptId as string,
+		})
+	// @ts-ignore
+	).results[0].code.rich_text;
+
+	console.log(textArray);
+	return {
+		props: {
+			transcriptHtml: textArray
+				.map((textObj) => {
+					return textObj.plain_text;
+				})
+				.join(""),
+		},
+	};
+}
